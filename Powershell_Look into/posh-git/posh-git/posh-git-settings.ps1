3f075a68158a811c3e527b3cf18598cacdb7c407
JayDeuce <jwdurant@gmail.com>
2016-02-24 21:53:55 -0500

    Removed posh-git module I was looking into

--- Powershell_Look into/posh-git/posh-git/posh-git-settings.ps1
+++
@@ -1,8 +0,0 @@
-$GitPromptSettings.BranchForegroundColor = "DarkGray"
-$GitPromptSettings.BranchIdenticalStatusToForegroundColor = "Green"
-$GitPromptSettings.BranchBehindAndAheadStatusForegroundColor = "Green"
-$GitPromptSettings.BranchBehindStatusForegroundColor = "Green"
-$GitPromptSettings.BranchAheadStatusForegroundColor = "Green"
-$GitPromptSettings.LocalWorkingStatusSymbol = '*'
-$GitPromptSettings.LocalStagedStatusSymbol = '*'
-$GitPromptSettings.BranchUntrackedSymbol = '*'
