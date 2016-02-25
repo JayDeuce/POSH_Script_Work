e8160f237393e8c169a085808f439beee0680f5f
JayDeuce <jwdurant@gmail.com>
2016-02-24 20:51:30 -0500

    Added Scripts to reveiw and eBook Library scripts to complete

---
+++ Powershell_Look into/posh-git/posh-git/posh-git-settings.ps1
@@ -0,0 +1,8 @@
+$GitPromptSettings.BranchForegroundColor = "DarkGray"
+$GitPromptSettings.BranchIdenticalStatusToForegroundColor = "Green"
+$GitPromptSettings.BranchBehindAndAheadStatusForegroundColor = "Green"
+$GitPromptSettings.BranchBehindStatusForegroundColor = "Green"
+$GitPromptSettings.BranchAheadStatusForegroundColor = "Green"
+$GitPromptSettings.LocalWorkingStatusSymbol = '*'
+$GitPromptSettings.LocalStagedStatusSymbol = '*'
+$GitPromptSettings.BranchUntrackedSymbol = '*'
