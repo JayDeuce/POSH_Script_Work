7d5146eb1c79b555302e8e5a665497fe230bbf06
JayDeuce <jwdurant@gmail.com>
2016-02-25 00:03:17 -0500

    1. Added Git prompt display functions. 2. Updated all double quotes to single quotes per POSH Standards. 3. Reorganized helper functions.

--- Powershell_Profile_Scripts/profile.ps1
+++ Powershell_Profile_Scripts/profile.ps1
@@ -1,6 +1,6 @@
 #region Set Windows Size and Shape in Console
 
-if ($host.Name -eq "ConsoleHost") {
+if ($host.Name -eq 'ConsoleHost') {
     # Set Variable
     $pshost = get-host
     $pswindow = $pshost.ui.rawui
@@ -20,33 +20,30 @@ if ($host.Name -eq "ConsoleHost") {
 
 #=======================================================================
 
-#region Set Colors of "Write-" Messages
+#region Set Colors of 'Write-' Messages
 
 $colors = $host.PrivateData
-$colors.VerboseForegroundColor = "white"
-$colors.VerboseBackgroundColor = "blue"
-$colors.WarningForegroundColor = "yellow"
-$colors.WarningBackgroundColor = "darkgreen"
+$colors.VerboseForegroundColor = 'white'
+$colors.VerboseBackgroundColor = 'blue'
+$colors.WarningForegroundColor = 'yellow'
+$colors.WarningBackgroundColor = 'darkgreen'
 # Comment out the next two lines to set the default Fore=Red, Back=black
-$colors.ErrorForegroundColor = "white"
-$colors.ErrorBackgroundColor = "red"
+$colors.ErrorForegroundColor = 'white'
+$colors.ErrorBackgroundColor = 'red'
 
 #endregion
 
 #=======================================================================
 
-#region Set PSModulePath and PSDrives
+#region Set PSDrives
 
-# Checks paths to my Coding folders, and sets "Coding" PSDrive and $env:PSModulesPath Variable to them if found
-# Allows loading of modules, not in the default directory. Defaults to "UserProfile" and no $env:PSModulesPath change.
-If (Test-Path -Path "Z:\Deuce\Dropbox\Jon\Coding") {
-    $env:PSModulePath = $env:PSModulePath + ";Z:\Deuce\Dropbox\Jon\Coding\SW_Config_Files\PowerShell\PSModules"
-    new-psdrive -name Coding -psprovider FileSystem -root "Z:\Deuce\Dropbox\Jon\Coding" | Out-Null
+# Checks paths to my Coding folders, and sets 'Coding' PSDrive to them if found
+If (Test-Path -Path 'Z:\Deuce\Dropbox\Jon\Coding') {
+    new-psdrive -name Coding -psprovider FileSystem -root 'Z:\Deuce\Dropbox\Jon\Coding' | Out-Null
     Set-Location Coding:
 }
-ElseIf (Test-Path -Path "F:\dropbox\jon\coding") {
-    $env:PSModulePath = $env:PSModulePath + ";F:\Dropbox\Jon\Coding\SW_Config_Files\PowerShell\PSModules"
-    new-psdrive -name Coding -psprovider FileSystem -root "F:\dropbox\jon\coding" | Out-Null
+ElseIf (Test-Path -Path 'F:\dropbox\jon\coding') {
+    new-psdrive -name Coding -psprovider FileSystem -root 'F:\dropbox\jon\coding' | Out-Null
     Set-Location Coding:
 }
 Else {
@@ -66,7 +63,7 @@ Else {
 #region System Module Directory
 
 # Load ActiveDirectory module from system modules directory if available
-if(get-module -ListAvailable -Name "ActiveDirectory") {
+if(get-module -ListAvailable -Name 'ActiveDirectory') {
     Import-Module ActiveDirectory
 }
 
@@ -92,33 +89,33 @@ function Start-ConsoleTranscript {
 
     # Check if I want to start a Transcript, Used if I am trying out commands
     [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
-    [string]$tranYN = [Microsoft.VisualBasic.Interaction]::MsgBox("Would you Like to Start a Transcript?", "YesNo, DefaultButton2",  "Start Transcript?")
-    # Check the answer for "Yes" or "No" and act upon the choice
+    [string]$tranYN = [Microsoft.VisualBasic.Interaction]::MsgBox('Would you Like to Start a Transcript?', 'YesNo, DefaultButton2',  'Start Transcript?')
+    # Check the answer for 'Yes' or 'No' and act upon the choice
     switch ($tranYN) {
         Yes {
             [string]$computerName = $env:COMPUTERNAME
             [string]$dateTime = get-date -format MM-dd-yyyy_HH-mm
-            [string]$logFileName = $computerName + "_" + $dateTime + ".txt"
+            [string]$logFileName = $computerName + '_' + $dateTime + '.txt'
             # Check for my Standard Coding Folder, and act accordingly to its presence or not
-            If (Test-Path -Path "Coding:\") {
-                start-transcript -path "Coding:\Source_Code\PowerShell\Transcripts\$logFileName" | Write-Host -BackgroundColor "darkgreen" -ForegroundColor "yellow"
+            If (Test-Path -Path 'Coding:\') {
+                start-transcript -path 'Coding:\Source_Code\PowerShell\Transcripts\$logFileName' | Write-Host -BackgroundColor 'darkgreen' -ForegroundColor 'yellow'
             }
             Else {
                 # If Standard Coding Path is not available, ask where to save
-                [string]$logPath = [Microsoft.VisualBasic.Interaction]::InputBox("Where would you like to save the transcript file?", "Trascript Path")
-                If ($logPath -eq "") { # Error handling if answer is blank or Cancel is hit
-                    Write-Warning "HEY!!!, No transcript save file location set, defaulting to Transcript Off!!"
-                    [Microsoft.VisualBasic.Interaction]::MsgBox("Transcript is off!", "OKOnly, Exclamation",  "Transcript Status") | Out-Null
+                [string]$logPath = [Microsoft.VisualBasic.Interaction]::InputBox('Where would you like to save the transcript file?', 'Trascript Path')
+                If ($logPath -eq '') { # Error handling if answer is blank or Cancel is hit
+                    Write-Warning 'HEY!!!, No transcript save file location set, defaulting to Transcript Off!!'
+                    [Microsoft.VisualBasic.Interaction]::MsgBox('Transcript is off!', 'OKOnly, Exclamation',  'Transcript Status') | Out-Null
                 }
                 else {
-                    $logPath = $logPath.TrimEnd("\")
+                    $logPath = $logPath.TrimEnd('\')
                     Check-IfNotPathCreate($logPath)
-                    start-transcript -path "$logPath\$logFileName" | Write-Host -BackgroundColor "darkgreen" -ForegroundColor "yellow"
+                    start-transcript -path '$logPath\$logFileName' | Write-Host -BackgroundColor 'darkgreen' -ForegroundColor 'yellow'
                 }
             }
         }
         No {
-            Write-Host "Transcripting is turned off for this console session." -BackgroundColor "darkgreen" -ForegroundColor "yellow"
+            Write-Host 'Transcripting is turned off for this console session.' -BackgroundColor 'darkgreen' -ForegroundColor 'yellow'
         }
     }
 }
@@ -129,7 +126,7 @@ function Start-RDP {
     param (
             [string]$ip
         )
-    Start-Process -FilePath mstsc -ArgumentList "/admin /w:1024 /h:768 /v:$ip"
+    Start-Process -FilePath mstsc -ArgumentList '/admin /w:1024 /h:768 /v:$ip'
 }
 # End Start-RDP Function
 #-----------------------------------------------------------------------
@@ -138,7 +135,7 @@ function Start-Chrome {
     param (
             [string]$webAddy
         )
-    if ($webAddy -eq "") {
+    if ($webAddy -eq '') {
         Start-Process -FilePath 'chrome.exe'
     }
     else {
@@ -155,40 +152,11 @@ function Goto-Desktop {
 #-----------------------------------------------------------------------
 # Goto-Repos Function
 function Goto-Repos {
-    Set-Location Coding:
+    Set-Location Coding:\Repos
 }
 # End Goto-Repos Function
 #-----------------------------------------------------------------------
-# Show-MyAliasList Function
-function Show-MyAliasList {
-    echo {
-            MY CUSTOM ALIASES
-
-            chrome  Start-Chrome
-            gh      Get-Help
-            gtd     Goto-Desktop
-            rdp     Start-RDP
-            repos   Goto-Repo
-            rt      Start-ConsoleTranscript
-            st      Stop-Transcript
-         }
-}
-# End Show-MyAliasList Function
-#-----------------------------------------------------------------------
-# Script Browser Setup
-if ($host.Name -eq "Windows PowerShell ISE Host") {
-    #Version: 1.3.2
-    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\System.Windows.Interactivity.dll'
-    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\ScriptBrowser.dll'
-    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\BestPractices.dll'
-    $scriptBrowser = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Browser', [ScriptExplorer.Views.MainView], $true)
-    $scriptAnalyzer = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Analyzer', [BestPractices.Views.BestPracticesView], $true)
-    $psISE.CurrentPowerShellTab.VisibleVerticalAddOnTools.SelectedAddOnTool = $scriptBrowser
-}
-# End Script Browser
-#-----------------------------------------------------------------------
 # Out-Notepad Function
-#requires -Version 2
 function Out-Notepad {
       param (
             [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
@@ -227,8 +195,64 @@ function Out-Notepad {
             $null = $type::SendMessage($child, 0x000C, 0, $text)
       }
 }
-# End Script Browser
+# End Out-Notepad Function
 #-----------------------------------------------------------------------
+# Start Get-GitInfoForDirectory Function
+function Get-GitInfoForDirectory {
+
+    param (
+    )
+
+    begin {
+        $gitBranch = (git branch)
+        $gitStatus = (git status)
+        $gitTextLine = ""
+    }
+
+    process {
+        try {
+            foreach ($branch in $gitBranch) {
+                if ($branch -match '^\* (.*)') {
+                    $gitBranchName = 'Git Repo - Branch: ' + $matches[1].ToUpper()
+    	        }
+            }
+    
+            if (!($gitStatus -like '*working directory clean*')) {
+                $gitStatusMark = ' ' + '/' + ' Status: ' + 'NEEDS UPDATING'
+            }
+            else {
+                $gitStatusMark = ' ' + '/' + ' Status: ' + 'UP TO DATE'
+            }
+        }
+        catch {
+        }
+    }
+
+    end {
+        if ($gitBranch) { 
+            $gitTextLine = "`n" + ' {' + $gitBranchName + $gitStatusMark + '}'            
+        }
+        return $gitTextLine       
+    }    
+}
+# End Get-GitInfoForDirectory Function
+#-----------------------------------------------------------------------
+# Show-MyAliasList Function
+function Show-MyAliasList {
+    Write-Output {
+            MY CUSTOM ALIASES/FUNCTIONS
+
+            chrome  Start-Chrome
+            gh      Get-Help
+            gtd     Goto-Desktop
+            notepad Out-Notepad
+            rdp     Start-RDP
+            repos   Goto-Repo
+            rt      Start-ConsoleTranscript
+            st      Stop-Transcript
+         }
+}
+# End Show-MyAliasList Function
 
 #endregion
 
@@ -238,18 +262,19 @@ function Out-Notepad {
 
 function prompt {
     # Sets the indicator to show role as User($) or Admin(#)
-    $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $Principal = new-object System.Security.principal.windowsprincipal($CurrentUser)
-    [string]$DTFormat = Get-Date -format "ddd MMM d | hh:mm:ss"
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = new-object System.Security.principal.windowsprincipal($currentUser)
+    [string]$dateFormat = Get-Date -format "ddd MMM d | hh:mm:ss"
 
     Write-Host "`n=======================================================================" -ForegroundColor Blue
-    Write-Host " ($DTFormat)" -ForegroundColor Yellow
-    if ($Principal.IsInRole("Administrators")) {
-        Write-Host " {#}-{$($(Get-Location).Path)}" -ForegroundColor Red
+    Write-Host " ($dateFormat)" -ForegroundColor Yellow
+    if ($principal.IsInRole('Administrators')) {
+        Write-Host " {#}-{$($(Get-Location).Path)}" -NoNewline -ForegroundColor Red        
     }
     else {
-        Write-Host " {$}-{$($(Get-Location).Path.replace($HOME,'~'))}" -ForegroundColor Green
+        Write-Host " {$}-{$($(Get-Location).Path.replace($HOME,'~'))}" -NoNewline -ForegroundColor Green        
     }
+    write-Host $(Get-GitInfoForDirectory) -ForegroundColor Magenta
     Write-Host $(if ($nestedpromptlevel -ge 1) { '>>' }) -NoNewline
     Write-Host "=======================================================================" -ForegroundColor Blue
     return "--> "
@@ -259,30 +284,46 @@ function prompt {
 
 #=======================================================================
 
-#region Set Aliases
+#region ISE Addon Configurations
 
-Set-Alias gh Get-Help
-Set-Alias st Stop-Transcript
-Set-Alias subl Edit-InSublime
-Set-Alias rdp Start-RDP
-Set-Alias chrome Start-Chrome
-Set-Alias gtd Goto-Desktop
-Set-Alias repos Goto-Repos
-Set-Alias ma Show-MyAliasList
-Set-Alias rt Start-ConsoleTranscript
+# Script Browser Setup
+if ($host.Name -eq 'Windows PowerShell ISE Host') {
+    #Version: 1.3.2
+    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\System.Windows.Interactivity.dll'
+    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\ScriptBrowser.dll'
+    Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\BestPractices.dll'
+    $scriptBrowser = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Browser', [ScriptExplorer.Views.MainView], $true)
+    $scriptAnalyzer = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Analyzer', [BestPractices.Views.BestPracticesView], $true)
+    $psISE.CurrentPowerShellTab.VisibleVerticalAddOnTools.SelectedAddOnTool = $scriptBrowser
+}
+# End Script Browser Setup
 
 #endregion
 
 #=======================================================================
 
-#region Call Commands/Functions
+#region Set Aliases
 
-# Clear Screen
-#cls
+Set-Alias chrome Start-Chrome
+Set-Alias gh Get-Help
+Set-Alias gtd Goto-Desktop
+Set-Alias ma Show-MyAliasList
+Set-Alias notepad Out-Notepad
+Set-Alias repos Goto-Repos
+Set-Alias rdp Start-RDP
+Set-Alias rt Start-ConsoleTranscript
+Set-Alias st Stop-Transcript
 
-# Start the console Transcript command
-if ($host.Name -eq "ConsoleHost") {
+#endregion
+
+#=======================================================================
+
+#region Command/Function Call
+
+if ($host.Name -eq 'ConsoleHost') {
     Start-ConsoleTranscript
+    Show-MyAliasList
 }
+
 #endregion
 
