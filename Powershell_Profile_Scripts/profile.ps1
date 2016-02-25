5d8a80d1f410ea7b065085ed073b6e77329f9657
JayDeuce <jwdurant@gmail.com>
2016-02-25 00:07:33 -0500

    Added new git status message to git functions when repo needs to publish commits

--- Powershell_Profile_Scripts/profile.ps1
+++ Powershell_Profile_Scripts/profile.ps1
@@ -220,6 +220,9 @@ function Get-GitInfoForDirectory {
             if (!($gitStatus -like '*working directory clean*')) {
                 $gitStatusMark = ' ' + '/' + ' Status: ' + 'NEEDS UPDATING'
             }
+            elseif ($gitStatus -like '*Your branch is ahead*') {
+                $gitStatusMark = ' ' + '/' + ' Status: ' + 'PUBLISH COMMITS'
+            }
             else {
                 $gitStatusMark = ' ' + '/' + ' Status: ' + 'UP TO DATE'
             }
